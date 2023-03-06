e1d31039647d34e56c970ef21ef329d3a8182fda
CodeSebb <SebastianSalvadorSanchez@gmail.com>
2023-03-05 22:33:48 -0800

    initial draft

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header-container">
+        <div className="header-style">
+            Shake The Dust.
+        </div>
+    </div>
+  )
+}
+
+export default Header
